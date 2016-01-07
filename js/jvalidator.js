1385a3dfcc1d2bfe74dda874ccf2f75744ccc127
JonathanCotta <JonathanCotta@users.noreply.github.com>
2016-01-06 22:43:59 -0200

    style.css changes adding tooltip

--- js/jvalidator.js
+++ js/jvalidator.js
@@ -4,20 +4,15 @@
         this.children(":input").each(function(){
             //attach event in each input
             $("[name="+this.name+"]").on("blur",function(){
-                if(!this.checkValidity()){
-                    invalid(this);
-                }
-                else {
-                    valid(this);
-                }
+                this.checkValidity() ? valid(this) : invalid(this);
             });
         });
         // what the you want to do in case of invalid input, feel free to add your code here.
-        var invalid = function(el , val){
+        var invalid = function(el){
             $(el).addClass('invalid');
         };
         // what the you want to do in case of valid input, feel free to add your code here.
-        var valid = function(el , val){
+        var valid = function(el){
             $(el).removeClass('invalid');
         }
     }

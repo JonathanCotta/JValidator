e8386f2f549c8c706e1e70f53ab80dc8e9f55c8b
JonathanCotta <JonathanCotta@users.noreply.github.com>
2016-01-06 22:48:11 -0200

    deleted local jquery and made changes all plugin design

--- js/jvalidator.js
+++ js/jvalidator.js
@@ -1,18 +1,37 @@
 (function($) {
-    $.fn.formValidation = function(){
-        // getting all the inputs in the form
+    // method to attach validation event in all inputs on form
+    $.fn.inputsValidation = function(){
         this.children(":input").each(function(){
-            //attach event in each input
-            $("[name="+this.name+"]").on("blur",function(){
-                this.checkValidity() ? valid(this) : invalid(this);
+            $("[name='"+this.name+"']").on("blur",function(){
+                this.checkValidity() ? action.onValid(this) : action.onInvalid(this);
             });
         });
-        // what the you want to do in case of invalid input, feel free to add your code here.
-        var invalid = function(el){
+    }
+    //method that when triggered will verify all input in a form
+    $.fn.formValidation = function(){
+        var valid = true;
+        this.children(":input").each(function(){
+            if (!this.checkValidity()) {
+                valid = action.onInvalid(this);
+            }
+        });
+        return valid;
+    }
+    // object containing private functions used in the plugin methods, feel free to add or change this functions
+    var action = {
+        // action taken when a input is invalid.
+        onInvalid: function(el){
+            var tooltip = el.nextElementSibling;
+            tooltip.style.visibility = "visible";
+            tooltip.innerHTML = el.validationMessage;
+            el.focus();
             $(el).addClass('invalid');
-        };
-        // what the you want to do in case of valid input, feel free to add your code here.
-        var valid = function(el){
+            return false;
+        },
+        // action taken when a input is valid.
+        onValid:function(el){
+            var tooltip = el.nextElementSibling;
+            tooltip.style.visibility = "hidden";
             $(el).removeClass('invalid');
         }
     }
